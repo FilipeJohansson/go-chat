be4fab54353eb0209755b551d38de377403134e5
Filipe Johansson <filipejohansson7@gmail.com>
2025-05-04 15:35:39 -0300

    start websocket

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"server/pkg/packets"
+	"log"
+	"net/http"
+	"server/internal/server"
+	"server/internal/server/clients"
+)
 
-	"google.golang.org/protobuf/proto"
+var (
+	port = flag.Int("port", 8080, "Port to listen on")
 )
 
 func main() {
-	data := []byte{8, 139, 6, 18, 16, 10, 14, 72, 101, 108, 108, 111, 44, 32, 70, 105, 108, 105, 112, 101, 33}
+	flag.Parse()
 
-	packet := &packets.Packet{}
-	err := proto.Unmarshal(data, packet)
+	// Define the chat hub
+	hub := server.NewHub()
+
+	// Define the handler for WebSocket connections
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		hub.Serve(clients.NewWebSocketClient, w, r)
+	})
+
+	go hub.Run()
+	addr := fmt.Sprintf(":%d", *port)
+
+	log.Printf("Starting server on %s", addr)
+	err := http.ListenAndServe(addr, nil)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to start server: %v", err)
 	}
-
-	fmt.Println(packet)
 }
